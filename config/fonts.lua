7bb0ffae5f06b7afe31813f0412ea8c78354748b
yotsugi <nonyotsugi@outlook.com>
2024-08-09 12:37:11 +0800

    调整fontsize

--- config/fonts.lua
+++ config/fonts.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local platform = require("utils.platform")
 
 local font = "IBM Plex Mono"
-local font_size = platform().is_mac and 12 or 9
+local font_size = platform().is_mac and 14 or 12
 
 return {
   font = wezterm.font(font),
